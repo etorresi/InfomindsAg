8f711008fbfb412170e4874353e9261703487124
Egidio Torresi <egidio.torresi@gmail.com>
2023-09-26 16:36:54 +0000

    Customer data structure interfaces

--- src/Frontend/src/pages/CustomerListPage.tsx
+++ src/Frontend/src/pages/CustomerListPage.tsx
@@ -1,3 +1,20 @@
+
+interface CustomerListQuery {
+    id: number;
+    name: string;
+    address: string;
+    email: string;
+    phone: string;
+    iban: string;
+    customerCategory: CustomerCategory;
+}
+
+interface CustomerCategory { 
+    id: number;
+    code: string;
+    description: string;
+}
+
 export default function CustomerListPage() {
     return <>
         <i>Todo customers</i>
