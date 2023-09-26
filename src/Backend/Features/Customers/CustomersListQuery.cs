fd3f028c0d042e2f7bd82565a81713900447449b
Egidio Torresi <egidio.torresi@gmail.com>
2023-09-26 16:14:49 +0000

    Query handler development for Customers

--- src/Backend/Features/Customers/CustomersListQuery.cs
+++ src/Backend/Features/Customers/CustomersListQuery.cs
@@ -24,3 +24,47 @@ public class CustomersListQueryResponseCustomerCategory
     public string Code { get; set; } = "";
     public string Description { get; set; } = "";
 }
+
+internal class CustomersListQueryHandler : IRequestHandler<CustomersListQuery, List<CustomersListQueryResponse>>
+{
+    private readonly BackendContext context;
+
+    public CustomersListQueryHandler(BackendContext context)
+    {
+        this.context = context;
+    }
+
+    public async Task<List<CustomersListQueryResponse>> Handle(CustomersListQuery request, CancellationToken cancellationToken)
+    {
+        var query = context.Customers.AsQueryable();
+        if (!string.IsNullOrEmpty(request.Name))
+            query = query.Where(q => q.Name.ToLower().Contains(request.Name.ToLower()));
+        if (!string.IsNullOrEmpty(request.Email))
+            query = query.Where(q => q.Email.ToLower().Contains(request.Email.ToLower()));
+
+        var data = await query.OrderBy(q => q.Name).ToListAsync(cancellationToken);
+        var result = new List<CustomersListQueryResponse>();
+
+        foreach (var item in data)
+        {
+            var resultItem = new CustomersListQueryResponse
+            {
+                Id = item.Id,
+                Name = item.Name,
+                Address = item.Address,
+                Email = item.Email,
+                Phone = item.Phone,
+                Iban = item.Iban,
+                
+                CustomerCategory = await context.CustomerCategories
+                    .Where(q => q.Id == item.CustomerCategoryId)
+                    .Select(q => new CustomersListQueryResponseCustomerCategory { Code = q.Code, Description = q.Description })
+                    .SingleOrDefaultAsync(cancellationToken),
+            };
+
+            result.Add(resultItem);
+        }
+
+        return result;
+    }
+}
