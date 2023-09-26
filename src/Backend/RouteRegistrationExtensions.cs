b8a44626b38a440ff3914c009928d84c40e4490f
Egidio Torresi <egidio.torresi@gmail.com>
2023-09-26 16:17:49 +0000

    Added route for customers

--- src/Backend/RouteRegistrationExtensions.cs
+++ src/Backend/RouteRegistrationExtensions.cs
@@ -1,3 +1,4 @@
+using Backend.Features.Customers;
 using Backend.Features.Employees;
 using Backend.Features.Suppliers;
 
@@ -11,5 +12,6 @@ static class RouteRegistrationExtensions
 
         apiGroup.MapGet("suppliers/list", async ([AsParameters]SupplierListQuery query, IMediator mediator) => await mediator.Send(query)).WithName("GetSuppliersList").WithOpenApi();
         apiGroup.MapGet("employees/list", async ([AsParameters]EmployeesListQuery query, IMediator mediator) => await mediator.Send(query)).WithName("GetEmployeesList").WithOpenApi();
+        apiGroup.MapGet("customers/list", async ([AsParameters]CustomersListQuery query, IMediator mediator) => await mediator.Send(query)).WithName("GetCustomersList").WithOpenApi();
     }
 }
